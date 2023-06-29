ee8708db483fb236a996d7983d2260d291106341
ricky fan <rickyhzfan@gmail.com>
2023-06-29 00:03:41 -0400

    cleared segment

--- boot.asm
+++ boot.asm
@@ -1,10 +1,19 @@
-org	0x7C00		; Boot code is loaded by BIOS at 0x7C00
+use16	
+;; 16 bit real mode
+	org	0x7C00		; Boot code is loaded by BIOS at 0x7C00
 
-bits	16		; Currently in 16 bit real mode
+	start:
+		jmp		loader
+	
+	loader:
+		xor		ax, ax		; Ensure data & extra segments are 0 to start
+		mov		ds, ax
+		mov		es, ax
 
-start:
-	cli		; Clear all interrupts
-	hlt		; halt the system
+		cli		; Clear all interrupts
+		hlt		; Halt the system
+		
 
-times 510-($-$$) db 0	; Pads out 0s until the 510th byte is reached
-dw 0xAA55		; BIOS magic number; Boot signiture
+	;; Boot sector magic
+	times 510-($-$$) db 0	; Pads out 0s until the 510th byte is reached
+	dw 0xAA55		; BIOS magic number; Boot signiture
