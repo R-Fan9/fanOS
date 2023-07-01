395c63936ab75ba7c58c649ce491572ae67e9a7c
ricky fan <rickyhzfan@gmail.com>
2023-07-01 17:57:06 -0400

    set up boot sector

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,42 +1,58 @@
-use16	
-;; 16 bit real mode
-	org	0x7C00		; Boot code is loaded by BIOS at 0x7C00
+bits 16                 ; 16 bits real mode
 
-	start:
-		jmp		loader
-	
-	loader:
-		xor		ax, ax		; Ensure data & extra segments are 0 to start
-		mov		ds, ax
-		mov		es, ax
+org 0x7C00
 
-	.reset:
-		mov		ah, 0x0		; Reset floppy disk function
-		mov		dl, 0		; floppy disk is drive 0
-		int		0x13		; call BIOS interrupt 0x13
-		jc		.reset		; if carry bit is 1, there is error. Try again
-	
-	.read:
-		mov		ah, 0x2		; Read sector function
-		mov		al, 1		; Read 1 sector
-		mov		ch, 1		; Read from track 1
-		mov		cl, 2		; Read from sector 2
-		mov		dh, 0		; Head number 0
-		mov		dl, 0		; floppy disk is drive 0
+start:
+    jmp     loader
 
-		int		0x13		; call BIOS interrupt 0x13
-		jc		.read		; if carry bit is 1, there is error. Try again
+print_str:
+    lodsb               ; move next byte from string from SI to AL
+    or      al, al      ; check if AL == 0
+    jz      print_done  ; yes - reached the end of the string, done printing
+    mov     ah, 0xE     ; no - print the character 
+    int     0x10
+    jmp     print_str
+    
+print_done:
+    ret
 
-		jmp		0x1000:0x0
-		
+loader:
+    xor     ax, ax
+    mov     ds, ax
+    mov     es, ax
 
-	;; Boot sector magic
-	times 510-($-$$) db 0	; Pads out 0s until the 510th byte is reached
-	dw 0xAA55		; BIOS magic number; Boot signiture
-	
-	; End of sector 1, beginning of sector 2 -----------------------------
+    mov     si, msg
+    call    print_str
 
-	org 0x1000
+    xor     ax, ax
 
-	cli		; Clear all interrupts
-	hlt		; Halt the system
+    cli
+    hlt
+
+.reset:
+    mov     ah, 0x0     ; read floppy disk function
+    mov     dl, 0x0     ; drive 0 is floppy disk
+    int     0x13
+    jc      .reset      ; carry flag is set - error try again
+
+    mov     ax, 0x1000  ; read sector into address 0x1000:0
+    mov     es, ax
+    xor     bx, bx
+
+.read:
+    mov     ah, 0x02    ; read sector function
+    mov     al, 1       ; read 1 sector
+    mov     ch, 1       ; track 1 
+    mov     cl, 2       ; sector 2
+    mov     dh, 0       ; head 0
+    mov     dl, 0       ; drive 0
+
+    int     0x13
+    jc      .read       ; carry flag is set - error try again
+
+    jmp     0x1000:0x0
+
+msg	db	"Welcome to My Operating System!", 0
+
+times 510-($-$$) db 0
+dw 0xAA55
