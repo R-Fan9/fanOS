01cac62a02d60c206d52c4e185206f92e101334c
ricky fan <rickyhzfan@gmail.com>
2023-08-22 14:44:16 -0400

    code clean up

--- src/kernel.c
+++ src/kernel.c
@@ -3,8 +3,6 @@
 #include "memory/physical_mmngr.h"
 #include "memory/virtual_mmngr.h"
 
-physical_addr virt_to_phys(virtual_addr addr);
-
 __attribute__((section("kernel_main"))) void kmain(void) {
   // clear_screen();
 
