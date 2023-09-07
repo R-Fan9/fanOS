dcf4737d7523f91304fa63ee3c150511a84011c1
ricky fan <rickyhzfan@gmail.com>
2023-09-07 15:16:56 -0400

    code clean up

--- src/include/userland.c
+++ src/include/userland.c
@@ -1,12 +1,9 @@
 #include "C/stdint.h"
 #include "C/stdio.h"
 #include "C/stdlib.h"
-#include "debug/display.h"
 
 void userland(void) {
-  clear_screen();
-
-  print_string((uint8_t *)"Hello user land");
+  printf("Hello userland!!, %d %x", 10, 16);
 
   while (1) {
   }
